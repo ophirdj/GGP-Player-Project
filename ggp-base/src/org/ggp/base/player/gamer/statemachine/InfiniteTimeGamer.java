0afc83444286501ed3df319492635d209c714b79
dylanrhodes <dylanr@stanford.edu>
2013-04-15 17:54:20 -0700

    Added CautiousGamer, which chooses the move which maximizes his average utility minus that of his opponents over the possible successive states. Also restored InfiniteTimeGamer to its original state.

--- ggp-base/src/org/ggp/base/player/gamer/statemachine/InfiniteTimeGamer.java
+++ ggp-base/src/org/ggp/base/player/gamer/statemachine/InfiniteTimeGamer.java
@@ -7,7 +7,6 @@ import org.ggp.base.player.gamer.exception.GameAnalysisException;
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
-import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
@@ -16,7 +15,6 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 public class InfiniteTimeGamer extends StateMachineGamer {
-	private final int MAX_STATE_VALUE = 100;
 
 	@Override
 	public StateMachine getInitialStateMachine() {
@@ -37,23 +35,27 @@ public class InfiniteTimeGamer extends StateMachineGamer {
 		StateMachine stateMachine = getStateMachine();
 		List<Move> legalMoves = stateMachine.getLegalMoves(getCurrentState(), getRole());
 		Map<Move, List<MachineState>> nextStates = stateMachine.getNextStates(getCurrentState(), getRole());
-		
+
 		Move bestMove = null;
-		int bestValue = MAX_STATE_VALUE;
-		
-		while(System.currentTimeMillis() < timeout) {
-			for (Move nextMove : legalMoves) {
-				for (MachineState state : nextStates.get(nextMove)) {
-					int nextValue = getStateValue(state);
-					
-					if (nextValue < bestValue) {
-						bestValue = nextValue;
-						bestMove = nextMove;
-					}
+		int bestValue = -1;
+
+		for (Move move : legalMoves) {
+			int minValue = 100;
+
+			for (MachineState state : nextStates.get(move)) {
+				int value = getStateValue(state);
+
+				if (value < minValue) {
+					minValue = value;
 				}
 			}
+
+			if (minValue > bestValue) {
+				bestMove = move;
+				bestValue = minValue;
+			}
 		}
-		
+
 		return bestMove;
 	}
 
@@ -76,31 +78,26 @@ public class InfiniteTimeGamer extends StateMachineGamer {
 	public String getName() {
 		return "InfiniteTime";
 	}
-	
-	/*
-	 * Returns lowest value of states which succeed currentState or -1
-	 * if an error is thrown
-	 */
+
 	private int getStateValue(MachineState currentState) {
 		StateMachine stateMachine = getStateMachine();
-		Role currentRole = getRole();
-		
+
 		if (stateMachine.isTerminal(currentState)) {
 			try {
-				return stateMachine.getGoal(currentState, currentRole);
+				return stateMachine.getGoal(currentState, getRole());
 			} catch (GoalDefinitionException e) {
 				System.err.println("Error: Goal definition exception");
 				e.printStackTrace();
 				return -1;
 			}
 		}
-		
+
 		Map<Move, List<MachineState>> nextStates;
 		List<Move> legalMoves;
-		
+
 		try {
-			nextStates = stateMachine.getNextStates(currentState, currentRole);
-			legalMoves = stateMachine.getLegalMoves(currentState, currentRole);
+			nextStates = stateMachine.getNextStates(currentState, getRole());
+			legalMoves = stateMachine.getLegalMoves(currentState, getRole());
 		} catch (MoveDefinitionException e) {
 			System.err.println("Error: Move definition exception");
 			e.printStackTrace();
@@ -110,18 +107,25 @@ public class InfiniteTimeGamer extends StateMachineGamer {
 			e.printStackTrace();
 			return -1;
 		}
-		
-		int minValue = MAX_STATE_VALUE;
-		
+
+		int bestValue = 0;
+
 		for (Move move : legalMoves) {
+			int minValue = 100;
+
 			for (MachineState nextState : nextStates.get(move)) {
-				int nextStateValue = getStateValue(nextState);
-				if (nextStateValue < minValue) {
-					minValue = nextStateValue;
+				int value = getStateValue(nextState);
+				if (value < minValue) {
+					minValue = value;
 				}
 			}
-		}	
-		return minValue > 0 ? minValue : 0;
+
+			if (minValue > bestValue) {
+				bestValue = minValue;
+			}
+		}
+
+		return bestValue;
 	}
-	
+
 }
