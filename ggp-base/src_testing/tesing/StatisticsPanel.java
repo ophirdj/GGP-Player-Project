6b861e1503e477e8868d413365d5a081c226c502
Ophir De Jager <ophirdj@gmail.com>
2013-10-03 18:32:55 +0300

    Testing Environment! New testing environment now available in source folder src_testing! We can now run lots of games and see the results (will save them to disk in next commit).

---
+++ ggp-base/src_testing/tesing/StatisticsPanel.java
@@ -0,0 +1,164 @@
+package tesing;
+
+import java.awt.BorderLayout;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SortOrder;
+import javax.swing.RowSorter.SortKey;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+import org.ggp.base.server.event.ServerMatchUpdatedEvent;
+import org.ggp.base.util.match.Match;
+import org.ggp.base.util.observer.Event;
+import org.ggp.base.util.observer.Observer;
+import org.ggp.base.util.ui.JLabelBold;
+
+@SuppressWarnings("serial")
+public final class StatisticsPanel extends JPanel implements Observer
+{
+	private final JTable statsTable;
+	private final TableRowSorter<TableModel> sorter;
+	
+	public StatisticsPanel()
+	{
+		super(new BorderLayout());
+		
+        DefaultTableModel model = new DefaultTableModel();		
+        model.addColumn("Player");
+        model.addColumn("Games Won");
+        model.addColumn("Games Lost");
+        model.addColumn("Games Tied");
+        model.addColumn("Total Score");
+        
+		statsTable = new JTable(model)
+		{
+			@Override
+			public boolean isCellEditable(int rowIndex, int colIndex)
+			{
+				return false;
+			}
+			@Override
+			public Class<?> getColumnClass(int colIndex) {
+				if (colIndex == 0) return String.class;
+				if (colIndex == 1) return Integer.class;
+				if (colIndex == 2) return Integer.class;
+				if (colIndex == 3) return Integer.class;
+				if (colIndex == 4) return Integer.class;
+				return Object.class;
+			}
+		};
+		statsTable.setShowHorizontalLines(true);
+		statsTable.setShowVerticalLines(true);
+		statsTable.getColumnModel().getColumn(1).setPreferredWidth(2);
+		statsTable.getColumnModel().getColumn(2).setPreferredWidth(2);
+		statsTable.getColumnModel().getColumn(3).setPreferredWidth(1);
+		sorter = new TableRowSorter<TableModel>(model);
+		sorter.setComparator(1, new Comparator<Integer>() {
+			public int compare(Integer a, Integer b) {
+				return a-b;
+			}
+		});
+		sorter.setComparator(2, new Comparator<Integer>() {
+			public int compare(Integer a, Integer b) {
+				return a-b;
+			}
+		});
+		sorter.setComparator(3, new Comparator<Integer>() {
+			public int compare(Integer a, Integer b) {
+				return a-b;
+			}
+		});
+		sorter.setComparator(4, new Comparator<Integer>() {
+			public int compare(Integer a, Integer b) {
+				return a-b;
+			}
+		});
+		sorter.setSortKeys(Arrays.asList(new SortKey[]{new SortKey(1, SortOrder.DESCENDING), new SortKey(2, SortOrder.DESCENDING), new SortKey(3, SortOrder.DESCENDING), new SortKey(4, SortOrder.DESCENDING)}));
+		statsTable.setRowSorter(sorter);
+
+		add(new JLabelBold("Statistics"), BorderLayout.NORTH);
+		add(new JScrollPane(statsTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+	}
+
+	public void observe(Event event)
+	{
+		if (!(event instanceof ServerMatchUpdatedEvent)) return;
+		Match match = ((ServerMatchUpdatedEvent) event).getMatch();
+		
+		if (!match.isCompleted()) return;
+		if (match.getMatchId().startsWith("Test")) return;
+		
+		List<Integer> goals = match.getGoalValues();
+		List<String> players = match.getPlayerNamesFromHost();
+		for (int i = 0; i < players.size(); i++) { if (players.get(i)==null) { players.set(i, "?"); } }
+
+		Set<String> playersToAdd = new HashSet<String>(players);
+		DefaultTableModel model = (DefaultTableModel) statsTable.getModel();
+		for (int i = 0; i < model.getRowCount(); i++) {
+			String rowPlayer = model.getValueAt(i, 0).toString();
+			int playerIndex = players.indexOf(rowPlayer);
+			if (playerIndex != -1) {
+				int numVictories = (Integer)model.getValueAt(i, 1);
+				int numDefeats = (Integer)model.getValueAt(i, 2);
+				int numTies = (Integer)model.getValueAt(i, 3);
+				int oldScore = (Integer)model.getValueAt(i, 4);
+				model.setValueAt(oldScore + goals.get(playerIndex), i, 4);
+				if(isVictory(goals.get(playerIndex), goals)) {
+					model.setValueAt(numVictories + 1, i, 1);
+				}
+				if (isDefeat(goals.get(playerIndex), goals)) {
+					model.setValueAt(numDefeats + 1, i, 2);
+				}
+				if(isTie(goals)) {
+					model.setValueAt(numTies + 1, i, 3);
+				}
+				playersToAdd.remove(rowPlayer);
+			}
+		}
+		for (String playerToAdd : playersToAdd) {
+			int playerIndex = players.indexOf(playerToAdd);
+			int numVictories = isVictory(goals.get(playerIndex), goals) ? 1 : 0;
+			int numDefeats = isDefeat(goals.get(playerIndex), goals) ? 1 : 0;
+			int numTies = isTie(goals) ? 1 : 0;
+			model.addRow(new Object[]{playerToAdd, numVictories, numDefeats, numTies, goals.get(players.indexOf(playerToAdd))});
+		}
+		sorter.sort();
+	}
+
+	private static boolean isVictory(Integer score, List<Integer> goals) {
+		for(Integer goal: goals) {
+			if(goal > score) {
+				return false;
+			}
+		}
+		return !isTie(goals);
+	}
+	
+	private static boolean isDefeat(Integer score, List<Integer> goals) {
+		for(Integer goal: goals) {
+			if(goal < score) {
+				return false;
+			}
+		}
+		return !isTie(goals);
+	}
+	
+	private static boolean isTie(List<Integer> goals) {
+		for(Integer goal: goals) {
+			if(goal != goals.get(0)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ ggp-base/src_testing/tesing/TestingPanel.java
@@ -0,0 +1,340 @@
+package tesing;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
+import javax.swing.SpinnerNumberModel;
+
+import org.ggp.base.apps.server.error.ErrorPanel;
+import org.ggp.base.apps.server.history.HistoryPanel;
+import org.ggp.base.apps.server.scheduling.SchedulingPanel;
+import org.ggp.base.apps.server.states.StatesPanel;
+import org.ggp.base.apps.server.visualization.VisualizationPanel;
+import org.ggp.base.server.GameServer;
+import org.ggp.base.util.crypto.BaseCryptography.EncodedKeyPair;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.game.GameRepository;
+import org.ggp.base.util.gdl.grammar.GdlPool;
+import org.ggp.base.util.match.Match;
+import org.ggp.base.util.presence.PlayerPresence;
+import org.ggp.base.util.presence.PlayerPresenceManager.InvalidHostportException;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+import org.ggp.base.util.ui.CloseableTabs;
+import org.ggp.base.util.ui.GameSelector;
+import org.ggp.base.util.ui.JLabelBold;
+import org.ggp.base.util.ui.NativeUI;
+import org.ggp.base.util.ui.PlayerSelector;
+
+@SuppressWarnings("serial")
+public final class TestingPanel extends JPanel implements ActionListener
+{    
+	static void createAndShowGUI(TestingPanel serverPanel, String title)
+	{
+		JFrame frame = new JFrame(title);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setPreferredSize(new Dimension(1400, 800));
+		frame.getContentPane().add(serverPanel);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args)
+	{        
+	    NativeUI.setNativeUI();
+	    GdlPool.caseSensitive = false;
+	
+		final TestingPanel testingPanel = new TestingPanel();
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
+		{
+
+			public void run()
+			{
+				createAndShowGUI(testingPanel, "Testing Server");
+			}
+		});
+	}
+	
+	private Game theGame;
+	private EncodedKeyPair signingKeys;
+	
+	private final JPanel managerPanel;
+	private final JTabbedPane matchesTabbedPane;	
+	private final JPanel gamePanel;
+	private final JPanel playersPanel;
+	
+	private final SchedulingPanel schedulingPanel;
+	private final StatisticsPanel statisticsPanel;
+
+	private final List<JComboBox> playerFields;
+	private final List<JLabel> roleLabels;
+	private final JButton runButton;
+
+	private final JSpinner startClockSpinner;
+	private final JSpinner playClockSpinner;
+	private final JSpinner numberOfGamesSpinner;
+	
+	private final JCheckBox shouldPublish;
+	private final JCheckBox shouldSave;
+	
+	private final GameSelector gameSelector;
+	private final PlayerSelector playerSelector;
+	private final JList playerSelectorList;
+
+	public TestingPanel()
+	{
+		super(new GridBagLayout());
+		
+		runButton = new JButton(runButtonMethod());
+		startClockSpinner = new JSpinner(new SpinnerNumberModel(30,5,600,1));
+		playClockSpinner = new JSpinner(new SpinnerNumberModel(15,5,300,1));
+		numberOfGamesSpinner = new JSpinner(new SpinnerNumberModel(1,1,9999,1));
+		matchesTabbedPane = new JTabbedPane();
+		
+		managerPanel = new JPanel(new GridBagLayout());
+		gamePanel = new JPanel(new GridBagLayout());
+		playersPanel = new JPanel(new GridBagLayout());
+
+		roleLabels = new ArrayList<JLabel>();
+		playerFields = new ArrayList<JComboBox>();
+		theGame = null;
+
+		shouldSave = new JCheckBox("Save match to disk?", false);
+		shouldPublish = new JCheckBox("Publish match to the web?", false);
+		
+		runButton.setEnabled(false);
+
+		gameSelector = new GameSelector();
+		playerSelector = new PlayerSelector();
+		playerSelectorList = playerSelector.getPlayerSelectorList();
+		
+		int nRowCount = 0;
+		gamePanel.add(new JLabelBold("Match Setup"), new GridBagConstraints(0, nRowCount++, 3, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 25, 5, 25), 0, 0));
+		gamePanel.add(new JLabel("Repository:"), new GridBagConstraints(0, nRowCount, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 1, 5), 5, 5));
+		gamePanel.add(gameSelector.getRepositoryList(), new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 1, 5), 5, 5));
+		gamePanel.add(new JLabel("Game:"), new GridBagConstraints(0, nRowCount, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(1, 5, 5, 5), 5, 5));
+		gamePanel.add(gameSelector.getGameList(), new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 5, 5), 5, 5));
+		gamePanel.add(new JLabel("Start Clock:"), new GridBagConstraints(0, nRowCount, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 1, 5), 5, 5));
+		gamePanel.add(startClockSpinner, new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 1, 5), 5, 5));
+		gamePanel.add(new JLabel("Play Clock:"), new GridBagConstraints(0, nRowCount, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(1, 5, 5, 5), 5, 5));
+		gamePanel.add(playClockSpinner, new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 5, 5), 5, 5));
+		gamePanel.add(new JLabel("Number Of Games:"), new GridBagConstraints(0, nRowCount, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(1, 5, 5, 5), 5, 5));
+		gamePanel.add(numberOfGamesSpinner, new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 5, 5), 5, 5));
+		gamePanel.add(shouldSave, new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 1, 5), 5, 5));
+		gamePanel.add(shouldPublish, new GridBagConstraints(1, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 5, 5), 5, 5));		
+		gamePanel.add(runButton, new GridBagConstraints(1, nRowCount, 1, 1, 0.0, 1.0, GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+		
+		nRowCount = 0;
+		playersPanel.add(new JLabelBold("Player List"), new GridBagConstraints(0, nRowCount++, 3, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 25, 5, 25), 0, 0));
+		playersPanel.add(new JScrollPane(playerSelectorList), new GridBagConstraints(0, nRowCount++, 3, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(5, 25, 5, 25), 0, 0));
+		playersPanel.add(new JButton(addPlayerButtonMethod()), new GridBagConstraints(0, nRowCount, 1, 1, 1.0, 1.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		playersPanel.add(new JButton(removePlayerButtonMethod()), new GridBagConstraints(1, nRowCount, 1, 1, 0.0, 1.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		playersPanel.add(new JButton(testPlayerButtonMethod()), new GridBagConstraints(2, nRowCount++, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		
+		nRowCount = 0;
+		managerPanel.add(gamePanel, new GridBagConstraints(0, nRowCount++, 1, 1, 1.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 5, 5));
+		managerPanel.add(new JSeparator(), new GridBagConstraints(0, nRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 5, 5));
+		managerPanel.add(playersPanel, new GridBagConstraints(0, nRowCount++, 1, 1, 1.0, 1.0, GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+
+		this.add(managerPanel, new GridBagConstraints(0, 0, 1, 1, 0.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+		this.add(matchesTabbedPane, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+		
+        gameSelector.getGameList().addActionListener(this);
+        gameSelector.repopulateGameList();
+        
+        schedulingPanel = new SchedulingPanel();
+        statisticsPanel = new StatisticsPanel();
+		matchesTabbedPane.addTab("Overview", new OverviewPanel());
+	}
+	
+	public void setSigningKeys(EncodedKeyPair keys) {
+		signingKeys = keys;
+	}
+	
+	class OverviewPanel extends JPanel {
+		public OverviewPanel() {
+			super(new GridBagLayout());
+			add(schedulingPanel, new GridBagConstraints(0, 0, 1, 1, 2.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+			add(statisticsPanel, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));					
+		}
+	}
+	
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == gameSelector.getGameList()) {
+            theGame = gameSelector.getSelectedGame();
+
+            for (int i = 0; i < roleLabels.size(); i++) {
+                gamePanel.remove(roleLabels.get(i));
+                gamePanel.remove(playerFields.get(i));
+            }
+
+            roleLabels.clear();
+            playerFields.clear();
+
+            validate();
+            runButton.setEnabled(false);
+            if (theGame == null)
+                return;            
+
+            StateMachine stateMachine = new ProverStateMachine();
+            stateMachine.initialize(theGame.getRules());
+            List<Role> roles = stateMachine.getRoles();
+            
+            int newRowCount = 8;
+            for (int i = 0; i < roles.size(); i++) {
+                roleLabels.add(new JLabel(roles.get(i).getName().toString() + ":"));
+                playerFields.add(playerSelector.getPlayerSelectorBox());
+                playerFields.get(i).setSelectedIndex(i%playerFields.get(i).getModel().getSize());
+
+                gamePanel.add(roleLabels.get(i), new GridBagConstraints(0, newRowCount, 1, 1, 1.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(1, 5, 1, 5), 5, 5));
+                gamePanel.add(playerFields.get(i), new GridBagConstraints(1, newRowCount++, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 1, 5), 5, 5));
+            }
+            gamePanel.add(runButton, new GridBagConstraints(1, newRowCount, 1, 1, 0.0, 1.0, GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+
+            validate();
+            runButton.setEnabled(true);
+        }        
+    }	
+
+	private AbstractAction runButtonMethod() {
+		return new AbstractAction("Start Experiment!") {
+			public void actionPerformed(ActionEvent evt) {
+				final int startClock = (Integer)startClockSpinner.getValue();
+				final int playClock = (Integer)playClockSpinner.getValue();
+				final int numberOfGames = (Integer)numberOfGamesSpinner.getValue();
+
+				final List<PlayerPresence> thePlayers = new ArrayList<PlayerPresence>();
+				for (JComboBox playerField : playerFields) {
+                	String name = playerField.getSelectedItem().toString();
+                	thePlayers.add(playerSelector.getPlayerPresence(name));
+				}
+				(new Thread(new Runnable() {
+					private List<PlayerPresence> players = thePlayers;
+					private int startTime = startClock;
+					private int playTime = playClock;
+					private int numGames = numberOfGames;
+					
+					@Override
+					public void run() {
+						while (numGames > 0) {
+							System.out.println(numGames);
+							GameServer gameServer = startTestingServer(theGame, players, "Base", startTime, playTime, shouldSave.isSelected(), shouldPublish.isSelected());
+							try {
+								gameServer.join();
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							//((JButton)(((JPanel)(matchesTabbedPane.getTabComponentAt(matchesTabbedPane.getTabCount() - 1))).getComponent(1))).doClick();
+							--numGames;
+						}
+					}
+				})).start();
+			}
+		};
+	}
+
+	private AbstractAction testPlayerButtonMethod() {
+		return new AbstractAction("Test") {
+			public void actionPerformed(ActionEvent evt) {
+				if (playerSelectorList.getSelectedValue() != null) {
+					Game testGame = GameRepository.getDefaultRepository().getGame("maze");
+					String playerName = playerSelectorList.getSelectedValue().toString();
+					List<PlayerPresence> thePlayers = Arrays.asList(new PlayerPresence[]{playerSelector.getPlayerPresence(playerName)});				
+					startTestingServer(testGame, thePlayers, "Test", 10, 5, false, false);
+				}
+			}
+		};
+	}
+	
+	private AbstractAction addPlayerButtonMethod() {
+		return new AbstractAction("Add") {
+			public void actionPerformed(ActionEvent evt) {
+				String hostport = JOptionPane.showInputDialog(null, "What is the new player's address?\nPlease use the format \"host:port\".", "Add a player", JOptionPane.QUESTION_MESSAGE, null, null, "127.0.0.1:9147").toString();
+				try {
+					playerSelector.addPlayer(hostport);
+				} catch (InvalidHostportException e) {
+					JOptionPane.showMessageDialog(null, "Could not parse the new player's address! Sorry.", "Error adding player", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		};
+	}
+	
+	private AbstractAction removePlayerButtonMethod() {
+		return new AbstractAction("Remove") {
+			public void actionPerformed(ActionEvent evt) {
+				if (playerSelectorList.getSelectedValue() != null) {
+					playerSelector.removePlayer(playerSelectorList.getSelectedValue().toString());
+				}
+			}
+		};
+	}
+
+	private GameServer startTestingServer(Game theGame, List<PlayerPresence> thePlayers, String matchIdPrefix, int startClock, int playClock, boolean shouldSave, boolean shouldPublish) {
+		try {
+			String matchId = matchIdPrefix + "." + theGame.getKey() + "." + System.currentTimeMillis();			
+			Match match = new Match(matchId, -1, startClock, playClock, theGame);
+
+			List<String> hosts = new ArrayList<String>(thePlayers.size());
+			List<Integer> ports = new ArrayList<Integer>(thePlayers.size());
+			List<String> playerNames = new ArrayList<String>(thePlayers.size());
+			for (PlayerPresence player : thePlayers) {
+                hosts.add(player.getHost());
+                ports.add(player.getPort());
+                playerNames.add(player.getName());
+			}
+			
+			match.setCryptographicKeys(signingKeys);
+			match.setPlayerNamesFromHost(playerNames);
+			
+			GameServer gameServer = new GameServer(match, hosts, ports);
+			gameServer.addObserver(schedulingPanel);
+			gameServer.addObserver(statisticsPanel);
+			gameServer.start();
+
+			if (shouldSave) {
+				File matchesDir = new File(System.getProperty("user.home"), "ggp-saved-matches");
+				if (!matchesDir.exists()) {
+					matchesDir.mkdir();
+				}
+				File matchFile = new File(matchesDir, match.getMatchId() + ".json");
+				gameServer.startSavingToFilename(matchFile.getAbsolutePath());
+			}
+			if (shouldPublish) {
+				if (!match.getGame().getRepositoryURL().contains("127.0.0.1")) {					
+					gameServer.startPublishingToSpectatorServer("http://matches.ggp.org/");
+					gameServer.setForceUsingEntireClock();
+				}				
+			}
+			return gameServer;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}	
+}
